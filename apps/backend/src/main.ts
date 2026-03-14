39070d51b221056adda3dac3cbd68918159a6014
Olena457 <olenademianenko42@gmail.com>
2026-03-14 22:13:54 +0200

    update cors origin for vercel

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,11 +13,13 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://application-six-ruby.vercel.app/', // render
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
   const config = new DocumentBuilder()
     .setTitle('Event Platform API')
     .setDescription('API documentation for the Event Platform application')
