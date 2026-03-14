9be06dddbed066ae2c9eab6fd06229658612e8eb
Olena457 <olenademianenko42@gmail.com>
2026-03-14 22:25:36 +0200

    Update main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'https://application-six-ruby.vercel.app/', // render
+      'https://application-six-ruby.vercel.app', // render
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
